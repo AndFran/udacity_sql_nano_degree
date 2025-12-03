45ef8ec9786a8f41258876c43deff63f86a7f829
AndFran <15264919+AndFran@users.noreply.github.com>
2025-12-03 09:59:30 +0100

    Update simple_aggregates.sql
    Also contains my MEDIAN calc (better), different from their solution which depends on know the number of rows.

--- simple_aggregates.sql
+++ simple_aggregates.sql
@@ -24,3 +24,48 @@ Your solution should use both aggregation and a mathematical operator.*/
 
 SELECT SUM(standard_amt_usd) / SUM(standard_qty) AS stan_price_per_unit
 FROM orders;
+
+
+/*When was the earliest order ever placed? You only need to return the date.*/
+SELECT MIN(occurred_at) AS earliest FROM orders;
+
+/*Try performing the same query as in question 1 without using an aggregation function.*/
+SELECT occurred_at FROM orders
+ORDER BY occurred_at ASC
+LIMIT 1;
+
+
+/*When did the most recent (latest) web_event occur?*/
+SELECT MAX(occurred_at) AS earliest FROM web_events
+
+/*Try to perform the result of the previous query without using an aggregation function.*/
+SELECT occurred_at FROM web_events
+ORDER BY occurred_at DESC
+LIMIT 1;
+
+/*Find the mean (AVERAGE) amount spent per order on each paper type, as well as the mean
+amount of each paper type purchased per order. Your final answer should have 6 values -
+one for each paper type for the average number of sales, as well as the average amount.*/
+SELECT COUNT(standard_amt_usd) / SUM(standard_amt_usd) AS avg_stand_usd,
+COUNT(gloss_amt_usd) / SUM(gloss_amt_usd) AS avg_gloss_usd,
+COUNT(poster_amt_usd) / SUM(poster_amt_usd) AS avg_poster_usd,
+
+COUNT(standard_qty) / SUM(standard_qty) AS avg_standard_qty,
+COUNT(gloss_qty) / SUM(gloss_qty) AS avg_gloss_qty,
+COUNT(poster_qty) / SUM(poster_qty) AS avg_poster_qty
+
+FROM orders;
+
+
+/*Via the video, you might be interested in how to calculate the MEDIAN. Though this
+ is more advanced than what we have covered so far try finding - what is the MEDIAN
+ total_usd spent on all orders?*/
+
+SELECT total AS median
+FROM
+(SELECT total,
+ROW_NUMBER() OVER(ORDER BY total) AS rn,
+COUNT(*) OVER() AS total_rows
+FROM orders
+ORDER BY total) AS stats
+WHERE stats.rn IN ( (total_rows + 1) / 2, (total_rows + 2) / 2)
