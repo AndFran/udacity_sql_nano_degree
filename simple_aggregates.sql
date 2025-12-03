41f9a47d425b717b43b702aae4362d6deda77ad5
AndFran <15264919+AndFran@users.noreply.github.com>
2025-12-03 10:33:30 +0100

    Update simple_aggregates.sql

--- simple_aggregates.sql
+++ simple_aggregates.sql
@@ -69,3 +69,81 @@ COUNT(*) OVER() AS total_rows
 FROM orders
 ORDER BY total) AS stats
 WHERE stats.rn IN ( (total_rows + 1) / 2, (total_rows + 2) / 2)
+
+
+
+
+/*Which account (by name) placed the earliest order? Your solution should have the account name
+and the date of the order.*/
+
+SELECT a.name, o.occurred_at
+FROM accounts a
+JOIN orders o
+ON a.id = o.account_id
+ORDER BY occurred_at
+LIMIT 1;
+
+
+/*Find the total sales in usd for each account. You should include two columns -
+the total sales for each company's orders in usd and the company name.*/
+SELECT a.name, SUM(o.total) as total
+FROM orders AS o
+INNER JOIN accounts AS a
+WHERE o.account_id = a.id
+GROUP BY a.name
+ORDER BY a.name
+
+
+/*Via what channel did the most recent (latest) web_event occur, which account was
+associated with this web_event? Your query should return only three values - the date,
+channel, and account name.*/
+
+SELECT w.occurred_at, w.channel, a.name
+FROM web_events w
+JOIN accounts a
+ON w.account_id = a.id
+ORDER BY w.occurred_at DESC
+LIMIT 1;
+
+
+/*Find the total number of times each type of channel from the web_events was used.
+Your final table should have two columns - the channel and the number of times the channel was used.*/
+SELECT channel, COUNT(channel) AS use_count
+FROM web_events
+GROUP BY channel;
+
+
+/*Who was the primary contact associated with the earliest web_event?*/
+SELECT a.primary_poc
+FROM web_events w
+JOIN accounts a
+ON a.id = w.account_id
+ORDER BY w.occurred_at
+LIMIT 1;
+
+
+/*What was the smallest order placed by each account in terms of total usd.
+Provide only two columns - the account name and the total usd. Order from smallest dollar
+amounts to largest.*/
+SELECT MIN(o.total) as lowest_amount_per_account, a.name
+FROM orders AS o
+INNER JOIN accounts AS a
+ON o.account_id = a.id
+GROUP BY a.name
+ORDER BY lowest_amount_per_account
+
+
+/*Find the number of sales reps in each region. Your final table should have two
+columns - the region and the number of sales_reps. Order from the fewest reps to most reps.*/
+SELECT r.name, COUNT(sr.id) AS sr_count
+FROM sales_reps AS sr
+INNER JOIN region AS r
+ON sr.region_id = r.id
+GROUP BY r.id, r.name
+ORDER BY sr_count ASC
+
+
+
+
+
+
