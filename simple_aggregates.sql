324a6adb974dc3eee6f57904dd3eb0f5575c4652
AndFran <15264919+AndFran@users.noreply.github.com>
2025-12-04 08:55:59 +0100

    Update simple_aggregates.sql

--- simple_aggregates.sql
+++ simple_aggregates.sql
@@ -144,6 +144,65 @@ ORDER BY sr_count ASC
 
 
 
+/*For each account, determine the average amount of each type of paper they purchased across their
+orders. Your result should have four columns - one for the account name and one for the average
+quantity purchased for each of the paper types for each account.*/
+SELECT a.name, a.id,
+AVG(o.standard_qty) AS standard_qty,
+AVG(o.gloss_qty) AS gloss_qty,
+AVG(o.poster_qty) AS poster_qty
+ FROM accounts AS a
+INNER JOIN orders AS o
+ON a.id = o.account_id
+GROUP BY a.name, a.id
+ORDER BY a.name;
+
+
+
+/*For each account, determine the average amount spent per order on each paper type.
+Your result should have four columns - one for the account name and one for the average
+amount spent on each paper type.*/
+SELECT a.name, a.id,
+AVG(o.standard_amt_usd) AS standard_amt_usd,
+AVG(o.gloss_amt_usd) AS gloss_amt_usd,
+AVG(o.poster_amt_usd) AS poster_amt_usd
+ FROM accounts AS a
+INNER JOIN orders AS o
+ON a.id = o.account_id
+GROUP BY a.name, a.id
+ORDER BY a.name;
+
+
+/*Determine the number of times a particular channel was used in the web_events table for
+each sales rep. Your final table should have three columns - the name of the sales rep,
+the channel, and the number of occurrences. Order your table with the highest number of
+occurrences first.*/
+SELECT sr.id, sr.name, COUNT(we.channel) AS channel_count
+FROM accounts AS a
+INNER JOIN web_events AS we
+ON a.id = we.account_id
+INNER JOIN sales_reps AS sr
+ON a.sales_rep_id = sr.id
+GROUP BY sr.id, sr.name
+ORDER BY channel_count DESC;
+
+
+
+
+
+/*Determine the number of times a particular channel was used in the web_events table
+for each region. Your final table should have three columns - the region name, the channel,
+and the number of occurrences. Order your table with the highest number of occurrences first.*/
+SELECT r.id, r.name, COUNT(we.channel) AS channel_count
+FROM accounts AS a
+INNER JOIN web_events AS we
+ON a.id = we.account_id
+INNER JOIN sales_reps AS sr
+ON a.sales_rep_id = sr.id
+INNER JOIN region AS r
+ON sr.region_id = r.id
+GROUP BY r.id, r.name
+ORDER BY channel_count DESC;
 
 
 
